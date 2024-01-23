df6c85b89548dcb73af162fcd2187cf92c5a2d38
Khanh <ngokhanh809@gmail.com>
2024-01-23 15:07:32 -0800

    added more asD tests

--- emutest/asD.asm
+++ emutest/asD.asm
@@ -4,13 +4,15 @@ start:
     moveq #-1, d0        ; shift test
     movea #1, sp
     asr.b #8, d0
-        beq.b failed ; assert Z clear
-        bcc.b failed ; assert C set
-        bvs.b failed ; assert V clear 
-        bpl.b failed ; assert N set 
+        beq failed ; assert Z clear
+        bcc failed ; assert C set
+        bvs failed ; assert V clear 
+        bpl failed ; assert N set 
         moveq #0, d2
         addx d2, d2
-        beq.b failed ; assert X set, see line above 
+        beq failed ; assert X set, see line above 
+        cmp.l #-1, d0
+        bne failed   ; assert d0 == -1
     movea #2, sp
     asr.w d0, d0        ; oversized shift test
         beq.b failed ; assert Z clear
@@ -20,6 +22,8 @@ start:
         moveq #0, d2
         addx d2, d2
         beq.b failed ; assert X set, see line above 
+        cmp.l #-1, d0
+        bne failed   ; assert d0 == -1
     movea #3, sp        ; shift by 0 test
     moveq #0, d3     ; clears C and X
     asr.l d3, d0
@@ -29,6 +33,8 @@ start:
         bpl.b failed ; assert N set 
         addx d2, d2
         beq.b failed ; assert x set, see line above 
+        cmp.l #-1, d0
+        bne.b failed ; assert d0 == -1
 
     movea #4, sp        ; shift arith left test
     moveq #-128, d0     ; 0xFFFFFF80 -> 0xFFFFFF00
@@ -37,10 +43,8 @@ start:
         bcc failed   ; assert C set
         bne failed   ; assert Z set
         bmi failed   ; assert N clear
-
-    movea #5, sp
-    moveq #-1, d0
-
+        cmp.l #$FFFFFF00, d0
+        bne failed   ; assert d0 == 0xFFFFFF00
 
     movea #0, sp     ; passed
 failed:
--- main.c
+++ main.c
@@ -172,14 +172,11 @@ int main(int argc, char **argv)
     MC68020 M68k = MC68020Init(Memory.Buffer, Memory.Size, LittleEndian);
     M68k.Read = MemRead;
     M68k.Write = MemWrite;
-    double Start = clock();
-    while (M68k.R[0] != 100000000ul) /* 100_000_000 */
+    while ('q' != GetInput())
     {
         MC68020Execute(&M68k);
-        //DumpState(&M68k);
+        DumpState(&M68k);
     }
-    double Elapsed = (clock() - Start) / CLOCKS_PER_SEC;
-    printf("\nElapsed: %f\n", Elapsed);
 
     free(Memory.Buffer);
     return 0;
